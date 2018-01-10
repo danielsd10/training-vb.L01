b3c283d776332b66a64ad57c53cb3613bbc6b697
Daniel Salas <danielsd10@gmail.com>
2018-01-10 00:21:58 -0500

    función área triángulo

--- AplicacionEjemplo/ModuloPrincipal.vb
+++ AplicacionEjemplo/ModuloPrincipal.vb
@@ -2,21 +2,35 @@
 
     Sub Main()
 
-        'Declarar variable
-        Dim Nombre As String
+        'Declarar variables
+        Dim Base, Altura, Area As Double
 
-        'Solicitar nombre
-        Console.WriteLine("Ingrese su nombre:")
+        'Solicitar valores
+        Console.WriteLine("Calcular área de triángulo")
 
-        'Asignar valor con texto ingresado por el usuario
-        Nombre = Console.ReadLine()
+        'Asignar base
+        Console.WriteLine("Ingrese el valor de la base:")
+        Base = CDbl(Console.ReadLine())
+
+        'Asignar altura
+        Console.WriteLine("Ingrese el valor de la altura:")
+        Altura = CDbl(Console.ReadLine())
+
+        'Calcular área
+        Area = CalcularAreaTriangulo(Base, Altura)
 
         'Mostrar nombre
-        Console.WriteLine("Hola {0}!", Nombre)
+        Console.WriteLine("El área del triángulo es {0}", Area)
 
         'Esperar que el usuario presione Enter
         Console.ReadLine()
 
     End Sub
+
+    Function CalcularAreaTriangulo(Base As Double, Altura As Double)
+        Dim Area As Double
+        Area = (Base * Altura) / 2
+        Return Area
+    End Function
 
 End Module

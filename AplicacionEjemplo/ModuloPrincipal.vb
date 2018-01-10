c1148e0021e3242fd715b8f38df613b56f326f86
Daniel Salas <danielsd10@gmail.com>
2018-01-09 23:34:40 -0500

    Hola Mundo

---
+++ AplicacionEjemplo/ModuloPrincipal.vb
@@ -0,0 +1,13 @@
+﻿Module ModuloPrincipal
+
+    Sub Main()
+
+        'Mostrar texto en la consola
+        Console.WriteLine("Hola Mundo!")
+
+        'Esperar que el usuario presione Enter
+        Console.ReadLine()
+
+    End Sub
+
+End Module
